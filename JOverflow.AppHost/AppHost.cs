6459d071e46bc2e1949f42b62d66468199465d7d
javidhasanzade <jh.grunger@gmail.com>
2026-02-06 00:48:11 +0100

    added new question service and configured in app host

--- JOverflow.AppHost/AppHost.cs
+++ JOverflow.AppHost/AppHost.cs
@@ -2,4 +2,8 @@ var builder = DistributedApplication.CreateBuilder(args);
 
 var keycloack = builder.AddKeycloak("keycloack", 6001).WithDataVolume("keycloack-data");
 
+var questionService = builder.AddProject<Projects.QuestionService>("question-service")
+    .WithReference(keycloack)
+    .WaitFor(keycloack);
+
 builder.Build().Run();
