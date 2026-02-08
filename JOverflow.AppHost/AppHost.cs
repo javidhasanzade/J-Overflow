ae376db8784ae4ebd353dbb6ea843fa657d29a11
javidhasanzade <jh.grunger@gmail.com>
2026-02-08 01:11:17 +0100

    postgres DB, entities for question service and a db context

--- JOverflow.AppHost/AppHost.cs
+++ JOverflow.AppHost/AppHost.cs
@@ -1,9 +1,21 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
-var keycloack = builder.AddKeycloak("keycloack", 6001).WithDataVolume("keycloack-data");
+#pragma warning disable ASPIRECERTIFICATES001
+var keycloak = builder.AddKeycloak("keycloack", 6001)
+    .WithoutHttpsCertificate()
+    .WithDataVolume("keycloak-data");
+#pragma warning restore ASPIRECERTIFICATES001
+
+var postgres = builder.AddPostgres("postgres", port: 5432)
+    .WithDataVolume("postgres-data")
+    .WithPgAdmin();
+
+var questionDb = postgres.AddDatabase("questionDb");
 
 var questionService = builder.AddProject<Projects.QuestionService>("question-service")
-    .WithReference(keycloack)
-    .WaitFor(keycloack);
+    .WithReference(keycloak)
+    .WithReference(questionDb)
+    .WaitFor(keycloak)
+    .WaitFor(questionDb);
 
 builder.Build().Run();
