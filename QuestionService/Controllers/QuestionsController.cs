887edf153592aed99739b255bcac3f79ca9116f3
javidhasanzade <jh.grunger@gmail.com>
2026-02-10 23:40:26 +0100

    get endpoints for questions and tags

--- QuestionService/Controllers/QuestionsController.cs
+++ QuestionService/Controllers/QuestionsController.cs
@@ -17,12 +17,12 @@ public class QuestionsController(QuestionDbContext dbContext) : ControllerBase
     public async Task<ActionResult<Question>> CreateQuestion([FromBody] CreateQuestionDto questionDto)
     {
         var validTags = await dbContext.Tags.Where(x => questionDto.Tags.Contains(x.Slug)).ToListAsync();
-        
+
         var missing = questionDto.Tags.Except(validTags.Select(x => x.Slug).ToList()).ToList();
-        
+
         if (missing.Count != 0)
             return BadRequest($"Invalid tags: {string.Join(", ", missing)}");
-        
+
         var userId = User.FindFirstValue(ClaimTypes.NameIdentifier);
         var userName = User.FindFirstValue("name");
 
@@ -39,7 +39,34 @@ public class QuestionsController(QuestionDbContext dbContext) : ControllerBase
 
         dbContext.Questions.Add(question);
         await dbContext.SaveChangesAsync();
-        
+
         return Created($"/questions/{question.Id}", question);
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<List<Question>>> GetQuestions(string? tag)
+    {
+        var query = dbContext.Questions.AsQueryable();
+
+        if (!string.IsNullOrEmpty(tag))
+        {
+            query = query.Where(x => x.TagSlugs.Contains(tag));
+        }
+
+        return await query.OrderByDescending(x => x.CreatedAt).ToListAsync();
+    }
+
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Question>> GetQuestion(string id)
+    {
+        var question = await dbContext.Questions.FindAsync(id);
+
+        if (question is null) return NotFound();
+
+        await dbContext.Questions.Where(x => x.Id == id)
+            .ExecuteUpdateAsync(setters => setters.SetProperty(x => x.ViewCount,
+                x => x.ViewCount + 1));
+        
+        return question;
     }
 }
