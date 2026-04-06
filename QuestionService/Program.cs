a3eacaee99edc036da6e7e22e9839013c994cd98
javidhasanzade <jh.grunger@gmail.com>
2026-04-06 23:26:49 +0200

    added RabbitMQ connection retry policy with exponential backoff using Polly, updated RabbitMQ and Keycloak versions in docker-compose, and refined environment variables for services

--- QuestionService/Program.cs
+++ QuestionService/Program.cs
@@ -1,8 +1,12 @@
+using System.Net.Sockets;
 using Microsoft.EntityFrameworkCore;
 using OpenTelemetry.Resources;
 using OpenTelemetry.Trace;
+using Polly;
 using QuestionService.Data;
 using QuestionService.Services;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Exceptions;
 using Wolverine;
 using Wolverine.RabbitMQ;
 
@@ -28,6 +32,27 @@ builder.Services.AddOpenTelemetry().WithTracing(traceProvideBuilder =>
 {
     traceProvideBuilder.SetResourceBuilder(ResourceBuilder.CreateDefault()
         .AddService(builder.Environment.ApplicationName)).AddSource("Wolverine");
+});
+
+var retryPolicy = Policy.Handle<BrokerUnreachableException>()
+    .Or<SocketException>()
+    .WaitAndRetryAsync(retryCount: 5,
+        retryAttempt => TimeSpan.FromSeconds(Math.Pow(2, retryAttempt)),
+        onRetry: (exception, timeSpan, retryCount) =>
+        {
+            Console.WriteLine($"Retry attempt {retryCount} failed. Retrying in {timeSpan.Seconds} seconds...");
+        });
+
+await retryPolicy.ExecuteAsync(async () =>
+{
+    var endpoint = builder.Configuration.GetConnectionString("messaging") ??
+                   throw new InvalidOperationException("messaging connection string not found");
+
+    var factory = new ConnectionFactory
+    {
+        Uri = new Uri(endpoint)
+    };
+    await using var connection = await factory.CreateConnectionAsync();
 });
 
 builder.Host.UseWolverine(options =>
